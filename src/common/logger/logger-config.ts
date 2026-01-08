d7dd0ef494fda82c672625272576fc96829f9b13
SuyashSoundatte <suyashsoundatte7@gmail.com>
2026-01-08 09:21:20 +0530

    logger pino-http error solved

--- src/common/logger/logger-config.ts
+++ src/common/logger/logger-config.ts
@@ -1,27 +1,26 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const LoggerConfig = {
-  level: isProd ? 'info' : 'debug',
+  pinoHttp: {
+    level: isProd ? 'info' : 'debug',
 
-  // // HTTP logger
-  // customProps: (req) => ({
-  //   context: 'HTTP',
-  //   requestId: req.id,
-  // }),
+    redact: {
+      paths: [
+        'req.headers.authorization',
+        'req.headers.cookie',
+      ],
+      remove: true,
+    },
 
-  // redact: {
-  //   paths: ['req.headers.authorization', 'req.headers.cookie'],
-  //   remove: true,
-  // },
-
-  transport: !isProd
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'yyyy-mm-dd HH:MM:ss.l',
-          ignore: 'pid,hostname',
-        },
-      }
-    : undefined,
+    transport: !isProd
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'yyyymmdd HH:MM:ss.l',
+            ignore: 'pid,hostname',
+          },
+        }
+      : undefined,
+  },
 };
