86accca7b6a5c1a4a4cadfb5c8f806c521c63b6a
Suyash Soundatte <suyashsoundatte7@gmail.com>
2026-01-15 16:46:26 +0530

    Enhance UserController with CRUD operations
    Refactor user controller to use async/await for user operations and add methods for finding, updating, and deleting users.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from './entity/user.entity';
 
 interface createUserDTO {
   name: string;
@@ -11,9 +12,51 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
-  creatUser(@Body() dto: createUserDTO) {
-    this.userService.createUser(dto);
-    return 'user created';
+  async create(@Body() user: User) {
+    await this.userService.createUser(user);
+    return {
+      message: 'User created successfully',
+    };
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    const user = await this.userService.getUserById(+id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User) {
+    const updated = await this.userService.updateUser({
+      ...user,
+      id: +id,
+    });
+
+    if (!updated) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { message: 'User updated' };
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    const deleted = await this.userService.deleteUser(+id);
+
+    if (!deleted) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { message: 'User deleted' };
   }
 }
