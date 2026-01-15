80593a5aa8b7beb00ab6797419ccc7967ea2df76
Suyash Soundatte <suyashsoundatte7@gmail.com>
2026-01-15 16:46:00 +0530

    Add ProcessErrorHandler to CommonModule providers

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './providers/database/database.module';
+import { ProcessErrorHandler } from './process/process-error.handler';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [ProcessErrorHandler],
+  exports: [ProcessErrorHandler]
 })
 export class CommonModule {}
